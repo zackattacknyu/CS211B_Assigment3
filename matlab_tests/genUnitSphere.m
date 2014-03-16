df903c95ceafdc54699be82a20fad172f7cc58c3
zackattacknyu <zackattacknyu@gmail.com>
2014-03-15 21:43:18 -0700

    figured out how to do proper sampling

--- matlab_tests/genUnitSphere.m
+++ matlab_tests/genUnitSphere.m
@@ -10,7 +10,7 @@ P = (
 %}
 clear all;
 numGridVals = 30;
-numIterations = 10;
+numIterations = 5;
 
 interval = 2/numGridVals;
 intervalValues = -1:interval:1;
@@ -28,6 +28,14 @@ mean = 1/2;
 
 %makes the meshgrid
 [gridXvals, gridYvals] = meshgrid(intervalValues,intervalValues);
+
+%the normal used to get the hemisphere we want
+normal = [1/sqrt(2),1/sqrt(2),0];
+%normal = [0,0,1];
+
+aVal = normal(1);
+bVal = normal(2);
+cVal = normal(3);
 
 for iteration = 1:numIterations
     
@@ -50,9 +58,25 @@ for iteration = 1:numIterations
     XvalsPlot = Xvals(squaredDist <= radius^2);
     YvalsPlot = Yvals(squaredDist <= radius^2);
     ZvalsPlot = sqrt(1 - XvalsPlot.^2 - YvalsPlot.^2);
+    
+    XvalsPlot = [XvalsPlot XvalsPlot];
+    YvalsPlot = [YvalsPlot YvalsPlot];
+    ZvalsPlot = [ZvalsPlot -ZvalsPlot];
+    
+    %{
+    To rotate the points, all points on the unit sphere are specified, 
+        and to do only the ones with respect to the normal, I used only
+        the vectors (a,b,c) where (a,b,c)(x,y,z) >= 0. 
+    %}
+    dotProds = XvalsPlot.*aVal + YvalsPlot.*bVal + ZvalsPlot.*cVal;
+    
+    XvalsToPlot = XvalsPlot(dotProds >= 0);
+    YvalsToPlot = YvalsPlot(dotProds >= 0);
+    ZvalsToPlot = ZvalsPlot(dotProds >= 0);
+    
 
     hold on
-    plot3(XvalsPlot,YvalsPlot,ZvalsPlot,'r.');
+    plot3(XvalsToPlot,YvalsToPlot,ZvalsToPlot,'r.');
     hold off
     
 end
